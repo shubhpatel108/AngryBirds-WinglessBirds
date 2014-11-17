161560004886ad2b8a48aa95ae2bb65e77d2e51a
Shubham Patel <shubhpatel108@gmail.com>
2014-11-17 19:24:51 +0530

    Integrate Learning with Naive Agent

--- demo/NaiveAgent.java
+++ demo/NaiveAgent.java
@@ -17,11 +17,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import ab.demo.HeuristicEngine;
 import ab.demo.other.ActionRobot;
 import ab.demo.other.Shot;
 import ab.planner.TrajectoryPlanner;
 import ab.utils.StateUtil;
+import ab.utils.ABUtil;
 import ab.vision.ABObject;
+import ab.vision.GameStateExtractor;
 import ab.vision.GameStateExtractor.GameState;
 import ab.vision.Vision;
 import ab.demo.StructureStudy;
@@ -38,6 +41,12 @@ public class NaiveAgent implements Runnable {
 	TrajectoryPlanner tp;
 	private boolean firstShot;
 	private Point prevTarget;
+
+    public ArrayList<Double> normalizedscorelist = new ArrayList<Double>();
+    public ArrayList<Integer> levellist = new ArrayList<Integer>();
+	public boolean isrepeating = false;
+    int repeatingcurrentlevel = 1;
+
 	// a standalone implementation of the Naive Agent
 	public NaiveAgent() {
 		
@@ -58,38 +67,87 @@ public class NaiveAgent implements Runnable {
 		aRobot.loadLevel(currentLevel);
 		while (true) {
 			GameState state = solve();
-			if (state == GameState.WON) {
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				int score = StateUtil.getScore(ActionRobot.proxy);
-				if(!scores.containsKey(currentLevel))
-					scores.put(currentLevel, score);
-				else
-				{
-					if(scores.get(currentLevel) < score)
+            if(isrepeating == false) {
+				if (state == GameState.WON) {
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					int score = StateUtil.getScore(ActionRobot.proxy);
+					if(!scores.containsKey(currentLevel))
 						scores.put(currentLevel, score);
-				}
-				int totalScore = 0;
-				for(Integer key: scores.keySet()){
+					else
+					{
+						if(scores.get(currentLevel) < score)
+							scores.put(currentLevel, score);
+					}
+					int totalScore = 0;
+					for(Integer key: scores.keySet()){
 
-					totalScore += scores.get(key);
-					System.out.println(" Level " + key
-							+ " Score: " + scores.get(key) + " ");
+						totalScore += scores.get(key);
+						System.out.println(" Level " + key
+								+ " Score: " + scores.get(key) + " ");
+					}
+					if(currentLevel == 8)
+                    {
+                        isrepeating = true;
+                        for(int i = 1;i <=normalizedscorelist.size(); i++)
+                        {
+                            levellist.add(i, i);
+                        }
+                        for(int i=0; i< normalizedscorelist.size()-1; i++){
+                            for(int j = i + 1; j < normalizedscorelist.size(); j++) {
+                                if(normalizedscorelist.get(i) > normalizedscorelist.get(j))
+                                {
+                                    int temp = levellist.get(i);
+                                    levellist.add(i, levellist.get(j));
+                                    levellist.add(j, temp);
+                                }
+                            }
+                        }
+                        aRobot.loadLevel(levellist.get(0));
+                    }
+                    else
+                    {
+                        aRobot.loadLevel(++currentLevel);
+                        // make a new trajectory planner whenever a new level is entered
+                        tp = new TrajectoryPlanner();
+                        // first shot on this level, try high shot first
+                        firstShot = true;
+                    }
 				}
-				System.out.println("Total Score: " + totalScore);
-				aRobot.loadLevel(++currentLevel);
-				// make a new trajectory planner whenever a new level is entered
-				tp = new TrajectoryPlanner();
-
-				// first shot on this level, try high shot first
-				firstShot = true;
-			} else if (state == GameState.LOST) {
-				System.out.println("Restart");
-				aRobot.restartLevel();
-			} else if (state == GameState.LEVEL_SELECTION) {
+			} else if (state == GameState.LOST)
+			{
+				if(currentLevel == 8)
+                    {
+                        isrepeating = true;
+                        for(int i = 1;i <=normalizedscorelist.size(); i++)
+                        {
+                            levellist.add(i, i);
+                        }
+                        for(int i=0; i< normalizedscorelist.size()-1; i++){
+                            for(int j = i + 1; j < normalizedscorelist.size(); j++) {
+                                if(normalizedscorelist.get(i) > normalizedscorelist.get(j))
+                                {
+                                    int temp = levellist.get(i);
+                                    levellist.add(i, levellist.get(j));
+                                    levellist.add(j, temp);
+                                }
+                            }
+                        }
+                        aRobot.loadLevel(levellist.get(0));
+                    }
+                    else {
+                        System.out.println("Restart");
+                        aRobot.loadLevel(++currentLevel);
+                    }
+			} 
+			else
+			{
+                aRobot.loadLevel(levellist.get(repeatingcurrentlevel++));
+			} 
+			if (state == GameState.LEVEL_SELECTION) {
 				System.out
 				.println("Unexpected level selection page, go to the last current level : "
 						+ currentLevel);
@@ -150,9 +208,10 @@ public class NaiveAgent implements Runnable {
 			if (!pigs.isEmpty()) {
 
 				Point finalReleasePoint = null;
+				GameStateExtractor stateExtractor = new GameStateExtractor();
+                int beginScore = stateExtractor.getScoreInGame(screenshot);
 				Shot shot = new Shot();
 				int dx,dy;
-				{
 					//ProbeBlocks(vision);
                     List<ABObject> woods = vision.getMBRVision().constructABObjects(vision.getMBRVision().findWoodMBR(), ABType.Wood);
                     List<ABObject> ices = vision.getMBRVision().constructABObjects(vision.getMBRVision().findIceMBR(),ABType.Ice);
@@ -160,31 +219,68 @@ public class NaiveAgent implements Runnable {
                     List<ABObject> TNTs = vision.getMBRVision().constructABObjects(vision.getMBRVision().findTNTsMBR(), ABType.TNT);
                     HeuristicEngine he = new HeuristicEngine(pigs, woods, ices, stones,TNTs, screenshot, aRobot.getBirdTypeOnSling(), sling);
 					StructureStudy ss = new StructureStudy(pigs, TNTs, ices, stones, woods);
-					Rectangle outerRectangle = ss.getStructureOutline();
-					double[][][] vectoredStructure = ss.calulate_vectors(outerRectangle);
-					he.makeAirBlocks(vectoredStructure,outerRectangle);
+                	boolean check = true;
 
-                    he.calcSupportFactor();
-                    he.calcDownwardFactor();
-                    he.calcDisplacementFactor();
-                    he.calcPenetrationFactor();
-                    he.calcWeakVicinity();
-                    ArrayList<ABObject> finalCandidateBlocks = he.computeFinalBlocks();
-
-                    ABObject blockToHit = he.filterFinalTarget(finalCandidateBlocks);
+                    ABObject blockToHit = GetTopPig(vision);
 //                    ABObject blockToHit = he.computeFinalBlocks2();
-                    System.out.println("BLOCK: " + blockToHit);
-                    if(blockToHit==null)
-                        blockToHit = GetTopPig(vision);
 
-                    Point targetPoint = new Point((int)blockToHit.getCenterX(),(int)(blockToHit.getCenterY()-(blockToHit.height/4)));
-                    System.out.println("target : " + targetPoint);
-                    System.out.println("sling : " + sling);
-                    List<Point> releasePoints = tp.estimateLaunchPoint(sling,targetPoint);
+                    List<Point> releasePoints;
+                	Point refPoint = tp.getReferencePoint(sling);
+
+	                if(pigs.size()==1)
+	                {
+	                    blockToHit = pigs.get(0);
+	                    releasePoints=tp.estimateLaunchPoint(sling,blockToHit.getCenter());
+	                    dx = (int)releasePoints.get(0).getX() - refPoint.x;
+	                    dy = (int)releasePoints.get(0).getY() - refPoint.y;
+	                    shot = new Shot(refPoint.x, refPoint.y, dx, dy, 0, 0);
+	                    if(ABUtil.isReachable(vision,blockToHit.getCenter(),shot))
+	                    {
+	                        finalReleasePoint = releasePoints.get(0);
+	                        check = false;
+	                    }
+	                    else
+	                    {
+	                        if(releasePoints.size()==2) {
+	                            finalReleasePoint = releasePoints.get(1);
+	                            check = false;
+	                        }
+	                    }
+	                }
+
+	                if(check)
+                	{
+    					Rectangle outerRectangle = ss.getStructureOutline();
+						double[][][] vectoredStructure = ss.calulate_vectors(outerRectangle);
+						he.makeAirBlocks(vectoredStructure,outerRectangle);
+	                    he.generateSubStructures();
+	                    he.calcSupportFactor();
+	                    he.calcDownwardFactor();
+	                    he.calcDisplacementFactor();
+	                    he.calcPenetrationFactor();
+	                    he.calcWeakVicinity();
+
+
+
+						ArrayList<ABObject> finalCandidateBlocks = he.computeFinalBlocks();
+
+                    	blockToHit = he.filterFinalTarget(finalCandidateBlocks);
+	                    if(isrepeating)
+	                    {
+	                        finalCandidateBlocks.remove(blockToHit);
+	                        blockToHit = he.filterFinalTarget(finalCandidateBlocks);
+	                    }
+
+	                    System.out.println("Block to hit: " + blockToHit);
+	                }
+	                Point targetPoint = blockToHit.getCenter();
+
+	                //Right now bottom up so choose low angle shot
+	                releasePoints = tp.estimateLaunchPoint(sling,targetPoint);
+	                System.out.println("releasepoints="+releasePoints.size());
 
                     if(releasePoints.size()==0)
                     {
-
                         {
                             System.out.println("No release point found for the target");
                             System.out.println("Try hitting Top pig");
@@ -192,61 +288,14 @@ public class NaiveAgent implements Runnable {
                             targetPoint = blockToHit.getCenter();
                             releasePoints = tp.estimateLaunchPoint(sling,targetPoint);
                         }
-
                     }
-                    finalReleasePoint = releasePoints.get(0);
-
-
+                    else
+	                    finalReleasePoint = releasePoints.get(0);
 
 //					ABObject pig = GetTopPig(vision);
-//
 //					Point _tpt = WeakJoint(vision, pig);
 
-					// if the target is very close to before, randomly choose a
-					// point near it
-					// if (prevTarget != null && distance(prevTarget, _tpt) < 10) {
-					// 	double _angle = randomGenerator.nextDouble() * Math.PI * 2;
-					// 	_tpt.x = _tpt.x + (int) (Math.cos(_angle) * 10);
-					// 	_tpt.y = _tpt.y + (int) (Math.sin(_angle) * 10);
-					// 	System.out.println("Randomly changing to " + _tpt);
-					// }
-//
-//					prevTarget = new Point(_tpt.x, _tpt.y);
-//
-//					// estimate the trajectory
-//					ArrayList<Point> pts = tp.estimateLaunchPoint(sling, _tpt);
-//
-//					// do a high shot when entering a level to find an accurate velocity
-//					if (firstShot && pts.size() > 1)
-//					{
-//						releasePoint = pts.get(0);
-//					}
-//					// do a high shot when entering a level to find an accurate velocity
-//					if (firstShot && pts.size() > 1)
-//					{
-//						releasePoint = pts.get(1);
-//					}
-//					else if (pts.size() == 1)
-//						releasePoint = pts.get(0);
-//					else if (pts.size() == 2)
-//					{
-//						// randomly choose between the trajectories, with a 1 in
-//						// 6 chance of choosing the high one
-//						if (randomGenerator.nextInt(6) == 0)
-//							releasePoint = pts.get(1);
-//						else
-//							releasePoint = pts.get(0);
-//					}
-//					else
-//						if(pts.isEmpty())
-//						{
-//							System.out.println("No release point found for the target");
-//							System.out.println("Try a shot with 45 degree");
-//							releasePoint = tp.findReleasePoint(sling, Math.PI/4);
-//						}
-//
-					// Get the reference point
-					Point refPoint = tp.getReferencePoint(sling);
+					refPoint = tp.getReferencePoint(sling);
 
 
 					//Calculate the tapping time according the bird type 
@@ -284,10 +333,10 @@ public class NaiveAgent implements Runnable {
 							System.err.println("No Release Point Found");
 							return state;
 						}
-				}
 
 				// check whether the slingshot is changed. the change of the slingshot indicates a change in the scale.
 				{
+					int maxscore = 0;
 					ActionRobot.fullyZoomOut();
 					screenshot = ActionRobot.doScreenShot();
 					vision = new Vision(screenshot);
@@ -303,12 +352,27 @@ public class NaiveAgent implements Runnable {
 								state = aRobot.getState();
 								if ( state == GameState.PLAYING )
 								{
+									if(firstShot == true)
+										maxscore = he.getMaximumScore();
 									screenshot = ActionRobot.doScreenShot();
 									vision = new Vision(screenshot);
 									List<Point> traj = vision.findTrajPoints();
 									tp.adjustTrajectory(traj, sling, finalReleasePoint);
 									firstShot = false;
+									stateExtractor = new GameStateExtractor();
+                                    int finalScore = stateExtractor.getScoreInGame(screenshot);
+                                    int scoreGained = finalScore - beginScore;
+                                    int totalScore = he.getMaximumScore();
 								}
+								else if(state==GameState.WON)
+                                {
+                                    screenshot = ActionRobot.doScreenShot();
+                                    stateExtractor = new GameStateExtractor();
+                                    int finalScore = stateExtractor.getScoreEndGame(screenshot);
+                                    int totalScore = he.getMaximumScore();
+                                    int scoreGained = finalScore - beginScore;
+                                    normalizedscorelist.add(currentLevel,(double)(100*finalScore)/maxscore);
+                                }
 							}
 						}
 						else
